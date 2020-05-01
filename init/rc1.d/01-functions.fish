6d1cf116d799963dd8a6cfde1387464aba9a0e6a
Andrew Matthews <andrew.matthews@team.telstra.com>
2020-05-01 16:24:32 +1000

    more tinkering

--- init/rc1.d/01-functions.fish
+++ init/rc1.d/01-functions.fish
@@ -1,71 +1,41 @@
 #!/usr/bin/env fish
 
-function nix -d "root command for FishDots Nixos"
-  if test 0 -eq (count $argv)
-    nix_help
-    return
-  end
-  switch $argv[1]
-    case fix
-      nix_fix $argv[2]
-    case repkg
-      nix_repkg $argv[2]
-    case clean
-      nix_clean
-    case install
-      nix_install
-    case update
-      nix_update $argv[2]
-    case search
-      nix_search $argv[2]
-    case upgrade
-      nix_upgrade $argv[2]
-    case update
-      nix_update $argv[2]
-    case '*'
-      nix_help
-  end
-end
+define_command fdnix "fishdots plugin for doing useful things with nixos"
 
-function nix_help -d "usage help"
-echo -e "nix usage:\n\n\n"
-echo -e "nix fix\n\tverify and repair store contents\n"
-echo -e "nix repkg\n\tbuild a package\n"
-echo -e "nix clean\n\trun nixos garbage collector\n"
-echo -e "nix search\n\tfind a package\n"
-echo -e "nix upgrade\n\tapply the updated nixos configuration and switch upgrading in the process\n"
-echo -e "nix update\n\tapply the updated nixos configuration and switch\n"
-echo -e "nix install\n\tinstall a package\n"
-
-end
-
+define_subcommand_nonevented fdnix fix nix_fix "verify and repair store contents"
+define_subcommand_nonevented fdnix repkg nix_repkg "build a package"
+define_subcommand_nonevented fdnix clean nix_clean "run nixos garbage collection"
+define_subcommand_nonevented fdnix search nix_search "find a package"
+define_subcommand_nonevented fdnix upgrade nix_upgrade "apply the updated nixos configuration and switch upgrading in the process"
+define_subcommand_nonevented fdnix update nix_update "apply the update configuration and switch"
+define_subcommand_nonevented fdnix install nix_install "install a package"
 
 function nixpaste -d ""
-    curl -F "text=<-" http://nixpaste.lbr.uno
+  curl -F "text=<-" http://nixpaste.lbr.uno
 end
 
 function nix_fix -d ""
-    nix-store --verify --check-contents --repair
+  sudo nix-store --verify --check-contents --repair
 end
 
 function nix_repkg -d ""
-    nix-build --check -A
+  sudo nix-build --check -A
 end
 
 function nix_clean -d ""
-    nix-collect-garbage -d
+  sudo nix-collect-garbage -d
 end
 
 function nix_search -a pattern -d "description"
-    nix-env -qaP --description \* | sed -re "s/^nixos\.//g" | fgrep -i $pattern
+  nix-env -qaP --description \* | sed -re "s/^nixos\.//g" | fgrep -i $pattern
 end
 
 function nix_update -d ""
-    nixos-rebuild switch
+  sudo nixos-rebuild switch
 end
 
 function nix_upgrade -d ""
-    nixos-rebuild switch --upgrade
+  sudo nixos-rebuild switch --upgrade
 end
 
 function nix_install -a pkg -d "install script"
